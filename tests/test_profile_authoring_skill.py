4a10b5fa7f686e8660f3463e7c9fedcf0dc67c77
Alex Merced <alex@alexmerced.dev>
2026-08-30 17:44:30 -0400

    Use tomli on Python 3.10

--- tests/test_profile_authoring_skill.py
+++ tests/test_profile_authoring_skill.py
@@ -1,5 +1,9 @@
 from pathlib import Path
-import tomllib
+
+try:
+    import tomllib
+except ModuleNotFoundError:  # pragma: no cover - exercised by the Python 3.10 CI job
+    import tomli as tomllib
 
 import yaml
 
